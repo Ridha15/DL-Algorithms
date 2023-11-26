fb8dfc276874d31403d60bdf5c75086a62e39061
Ridha15 <ridha.ds22@duk.ac.in>
2023-11-26 10:30:34 +0530

    app changes

--- app.py
+++ app.py
@@ -17,30 +17,28 @@ st.title("Deep Learning Algorithms")
 # Layout for buttons
 button_tumor_detection = st.button("Tumor Detection")
 button_sentiment_classification = st.button("Sentiment Classification")
-
-if button_tumor_detection:
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     # Display the uploaded image
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Add a "Predict" button
-    if st.button("Predict"):
-        st.write("Predict button clicked")
+if st.button("Predict"):
+    st.write("Predict button clicked")
 
         # Check if an image is uploaded before attempting to process it
-        if uploaded_image is not None:
-            model = load_model("models/cnn_model.h5")
+    if uploaded_image is not None:
+        model = load_model("models/cnn_model.h5")
             # Preprocess the image for tumor detection
-            processed_image = preprocess_image(uploaded_image)
+        processed_image = preprocess_image(uploaded_image)
             # Make the prediction
-            result = model.predict(processed_image)
+        result = model.predict(processed_image)
             # Display the result
-            if result[0][0] > 0.5:  # Assuming binary classification
-                st.write("Tumor Detected")
-            else:
-                st.write("No Tumor")
+        if result[0][0] > 0.5:  # Assuming binary classification
+            st.write("Tumor Detected")
         else:
-            st.warning("Please upload an image before clicking 'Predict'")
+            st.write("No Tumor")
+    else:
+        st.warning("Please upload an image before clicking 'Predict'")
