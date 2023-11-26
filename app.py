6da53f396b0eb40607fa049ae7d3a23bc547d7ba
Ridha K P <125472355+Ridha15@users.noreply.github.com>
2023-11-26 17:09:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,38 +2,40 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
-from io import BytesIO
 import cv2
+
+from transformers import AutoModelForImageClassification, AutoImageFeatureExtractor
+
 import requests
-import h5py
-
-# Function to preprocess image for tumor detection
-def preprocess_image(image_path, target_size=(180, 180)):
-    img = Image.open(image_path)
-    img = img.resize(target_size)
-    img = np.array(img) / 255.0  # Normalize pixel values to between 0 and 1
-    img = np.expand_dims(img, axis=0)
-    return img
 
 
-def load_model_from_google_drive(gdrive_file_id):
-    gdrive_url = f"https://drive.google.com/uc?id={gdrive_file_id}"
-    response = requests.get(gdrive_url)
-    
-    # Load the model from BytesIO using h5py
-    with h5py.File(BytesIO(response.content), 'r') as f:
-        model = load_model(f)
+# Function to load the model
+def load_model():
+    model_name = "Ridha15/cnn_model"  # Replace with your Hugging Face repo name
+    model = AutoModelForImageClassification.from_pretrained(model_name)
+    feature_extractor = AutoImageFeatureExtractor.from_pretrained(model_name)
+    return model, feature_extractor
 
-    return model
 
-# Google Drive File ID for your model file
-model_gdrive_file_id = '1_mKNM-t6Do7fmXtzrsE9F5L9DliR1Yas'
+model, feature_extractor = load_model()
 
-# Google Drive File ID for your model file
-model_gdrive_file_id = '1_mKNM-t6Do7fmXtzrsE9F5L9DliR1Yas'
+# User input for image upload
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Load the model
-
+if st.button("Predict"):
+    if uploaded_image:
+        # Load and preprocess the image
+        image = Image.open(uploaded_image)
+        inputs = feature_extractor(images=image, return_tensors="pt")
+        
+        # Make predictions
+        outputs = model(**inputs)
+        predicted_class = np.argmax(outputs.logits[0])
+        
+        # Display the result
+        st.success(f"The predicted class is: {predicted_class}")
+    else:
+        st.warning("Please upload an image")
 
 # Main content
 st.title("Deep Learning Algorithms")
@@ -52,21 +54,19 @@ if selected_option == "Tumor Detection":
         image = Image.open(uploaded_image)
         st.image(image, caption="Uploaded Image", use_column_width=True,width=10)
 
-    # Add a "Predict" button
-    if st.button("Predict"):
 
-        # Check if an image is uploaded before attempting to process it
-        if uploaded_image is not None:
-            model = load_model_from_google_drive(model_gdrive_file_id)
-            img=cv2.imread(uploaded_image)
-            img=Image.fromarray(img)
-            img=img.resize((128,128))
-            img=np.array(img)
-            input_img = np.expand_dims(img, axis=0)
-            res = model.predict(input_img)
-            if res:
-                print("Tumor Detected")
-            else:
-                print("No Tumor")
+
+    if st.button("Predict"):
+        if uploaded_image:
+            # Load and preprocess the image
+            image = Image.open(uploaded_image)
+            inputs = feature_extractor(images=image, return_tensors="pt")
+            
+            # Make predictions
+            outputs = model(**inputs)
+            predicted_class = np.argmax(outputs.logits[0])
+            
+            # Display the result
+            st.success(f"The predicted class is: {predicted_class}")
         else:
-            st.warning("Please upload an image before clicking 'Predict'")
+            st.warning("Please upload an image")
