636ad33d37b871b0702c0e014bcdf0d743ef80a9
Ridha15 <ridha.ds22@duk.ac.in>
2023-11-26 10:24:18 +0530

    app changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 
 # Function to preprocess image for tumor detection
@@ -22,30 +21,26 @@ button_sentiment_classification = st.button("Sentiment Classification")
 if button_tumor_detection:
     uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Display the uploaded image using matplotlib
+    # Display the uploaded image
     if uploaded_image is not None:
         image = Image.open(uploaded_image)
-        plt.imshow(image)
-        plt.axis("off")
-        st.pyplot(plt)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-# Add a "Predict" button
+    # Add a "Predict" button
     if st.button("Predict"):
         st.write("Predict button clicked")
 
         # Check if an image is uploaded before attempting to process it
         if uploaded_image is not None:
             model = load_model("models/cnn_model.h5")
-                # Preprocess the image for tumor detection
+            # Preprocess the image for tumor detection
             processed_image = preprocess_image(uploaded_image)
-                # Make the prediction
+            # Make the prediction
             result = model.predict(processed_image)
-                # Display the result
+            # Display the result
             if result[0][0] > 0.5:  # Assuming binary classification
                 st.write("Tumor Detected")
             else:
                 st.write("No Tumor")
-            
         else:
             st.warning("Please upload an image before clicking 'Predict'")
-
