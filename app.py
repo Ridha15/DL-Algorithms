171c7308e30befe39fce50bef9766e85a3f81ccd
Ridha15 <ridha.ds22@duk.ac.in>
2023-11-26 10:39:09 +0530

    app changes

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ if button_tumor_detection:
 
     # Display the uploaded image
     if uploaded_image is not None:
+        st.write("hello")
         image = Image.open(uploaded_image)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
